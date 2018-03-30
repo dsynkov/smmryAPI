f1026f86f968a10564540e731638acf9ed2fe21b
Dmitriy Synkov <dmitriysynkov@gmail.com>
2018-03-29 21:10:11 -0400

    Add subdirectories

--- smmryapi.py
+++ smmryapi.py
@@ -1,8 +1,8 @@
 import requests
 import tldextract
 import collections
-import re
 
+from summary import Summary
 
 class SmmryAPIException(Exception):
     pass
@@ -41,45 +41,3 @@ class SmmryAPI:
         smmry_dict['sm_api_content'] = smmry_dict['sm_api_content'].strip()
 
         return Summary(smmry_dict, params, response)
-
-
-class Summary:
-
-    def __init__(self, smmry_dict, params, response):
-
-        self.domain = self.get_domain_name(params)
-        self.params = params
-        self.url = response.url
-        self.smmry_dict = smmry_dict
-        self.requests_remaining = self.requests_remaining(smmry_dict)
-
-        for key, value in smmry_dict.items():
-            setattr(self, key, value)
-
-        self.length = 7  # Default
-        if params.get('SM_LENGTH'):
-            self.length = params['SM_LENGTH']
-
-    def __str__(self):
-        return self.sm_api_content
-
-    def __len__(self):
-        return self.length
-
-    def __iter__(self):
-        for key, value in self.smmry_dict.items():
-            yield (key, value)
-
-    def get_domain_name(self, params):
-
-        extract = tldextract.extract(params['SM_URL'])
-
-        return '.'.join([extract.domain, extract.suffix])
-
-    def requests_remaining(self, smmry_dict):
-
-        pattern = r'\d+'
-        message = smmry_dict['sm_api_limitation'].split(',')[1]
-        match = re.search(pattern, message)
-
-        return int(match.group())
---
+++ summary.py
@@ -0,0 +1,46 @@
+import re
+
+class Summary:
+
+    def __init__(self, smmry_dict, params, response):
+
+        self.params = params
+        self.url = response.url
+        self.smmry_dict = smmry_dict
+
+        self.sm_domain = self.get_domain_name(params)
+        self.sm_requests_remaining = self.requests_remaining(smmry_dict)
+
+        self.smmry_dict['sm_domain'] = self.sm_domain
+        self.smmry_dict['sm_requests_remaining'] = self.sm_requests_remaining
+
+        for key, value in smmry_dict.items():
+            setattr(self, key, value)
+
+        self.length = 7  # Default
+        if params.get('SM_LENGTH'):
+            self.length = params['SM_LENGTH']
+
+    def __str__(self):
+        return self.sm_api_content
+
+    def __len__(self):
+        return self.length
+
+    def __iter__(self):
+        for key, value in self.smmry_dict.items():
+            yield (key, value)
+
+    def get_domain_name(self, params):
+
+        extract = tldextract.extract(params['SM_URL'])
+
+        return '.'.join([extract.domain, extract.suffix])
+
+    def requests_remaining(self, smmry_dict):
+
+        pattern = r'\d+'
+        message = smmry_dict['sm_api_limitation'].split(',')[1]
+        match = re.search(pattern, message)
+
+        return int(match.group())
